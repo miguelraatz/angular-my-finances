86d25d69bad0465113598a65013aa47744931c16
miguelraatz <miguelraatz_@hotmail.com>
2023-08-04 14:48:21 -0300

    feat: add http-base service

---
+++ src/app/shared/base/http-base.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpBaseService {
+  public readonly httpClient!: HttpClient;
+
+  private apiBase = 'http://localhost:3000'
+
+  constructor(private readonly injector: Injector) {
+    if (injector == null || injector == undefined) {
+      throw new Error('Injector não pode ser nulo')
+    }
+
+    this.httpClient = injector.get(HttpClient);
+  }
+
+  public httpGet(endpoint: string): Observable<any> {
+    return this.httpClient.get(`${this.apiBase}${endpoint}`);
+  }
+
+  public httpPost(endpoint: string, payload: any): Observable<any> {
+    return this.httpClient.post(`${this.apiBase}${endpoint}`, payload);
+  }
+  public httpPut(endpoint: string, payload: any): Observable<any> {
+    return this.httpClient.put(`${this.apiBase}${endpoint}`, payload);
+  }
+  public httpDelete(endpoint: string): Observable<any> {
+    return this.httpClient.delete(`${this.apiBase}${endpoint}`);
+  }
+}
